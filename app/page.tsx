d79722411eebf8e19f03a53187c8e1b908a622be
KriNog <knogobods@kasko.io>
2025-09-13 09:38:19 +0300

    Page comments

--- app/page.tsx
+++ app/page.tsx
@@ -22,14 +22,7 @@ export default async function Home() {
             {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
           </div>
         </nav>
-        {/* <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5"> */}
         <PricingPage />
-        {/* <Hero />
-          <main className="flex-1 flex flex-col gap-6 px-4">
-            <h2 className="font-medium text-xl mb-4">Next steps</h2>
-            {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-          </main> */}
-        {/* </div> */}
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
           <p>
