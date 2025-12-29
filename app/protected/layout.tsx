4d9131f09a822a0dd1a1ee4684b5dbab9f8ad5f3
KriNog <knogobods@kasko.io>
2025-12-29 12:50:55 +0200

    Layout fix

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -11,7 +11,7 @@ export default function ProtectedLayout({
       <Analytics/>
       <div className="flex-1 w-full flex flex-col items-center">
         <nav className="w-full bg-[#f5f5f5] flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+          <div className="w-full flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
               {/* <Link href={"/"}>Next.js Supabase Starter</Link> */}
               <div className="flex items-center gap-2">
